17fd8d846cff2dfd72e52c80b6eb5cebb5cdbc8e
Daniel Carroll <Dancarrollsp@gmail.com>
2024-10-13 23:30:18 +0100

    third commit

--- PlayerController_Base.cs
+++ PlayerController_Base.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 10f; // increased speed
 
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f); // changed movement vector
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         transform.Translate(movement * speed * Time.deltaTime);
     }
 }
